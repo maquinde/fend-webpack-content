d491136c0bca716dfab574c730e2ce94c6f3cd40
Mike <maquinde@outlook.com>
2020-06-20 16:03:29 -0700

    completed tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,3 @@ const webpack = require('webpack');
 module.exports = {
 
 }
-
-
-// ?????
-// Lastly, verify the dependency for the development in "devDependencies" block as:
-
-// "devDependencies":{
-//   "webpack-dev-server": "^3.11.0",
-// },
